01466ad22a9cefe293be0b3f6b1c66e6b04e5f1c
Brian Lavery <brian.lavery@yahoo.co.uk>
2022-10-02 10:06:21 +0700

    added grid structure

--- src/components/App.js
+++ src/components/App.js
@@ -13,8 +13,9 @@ const App = () => {
 			<CssBaseline />
 			<BackgroundImage>
 				<div style={{ paddingTop: 100 }}>
-					<Grid container spacing={2} sx={{ flexGrow: 1 }}>
+					<Grid container spacing={2}>
 						<Grid
+							item
 							xs={10}
 							sm={6}
 							md={4}
@@ -28,25 +29,33 @@ const App = () => {
 							alignItems='center'>
 							<LogoImage />
 						</Grid>
-						<Grid xs={1} sm={3} md={4} lg={5} />
+						<Grid item xs={1} sm={3} md={4} lg={5} />
 
-						<Grid xs={12} xl={6} xlOffset={3} display='flex' justifyContent='center' alignItems='center'>
-							<Typography variant='h2' style={{ color: 'white' }}>
+						<div style={{ width: '100%', marginTop: 40 }}></div>
+
+						<Grid item xs={12} xl={6} xlOffset={3} display='flex' justifyContent='center' alignItems='center'>
+							<Typography variant='h2' style={{ color: 'white', fontWeight: 'bold' }}>
 								Thai coffee beans straight from the farmer
 							</Typography>
 						</Grid>
-						<Grid xs={0} xl={3} />
+						<Grid item xs={0} xl={3} />
 
-						<Grid xs={12} display='flex' justifyContent='center' alignItems='center'>
-							<Button variant='contained'>Pre-Order Now</Button>
-						</Grid>
+						<div style={{ width: '100%', marginTop: 40 }}></div>
 
-						<Grid xs={12} xl={6} xlOffset={3} display='flex' justifyContent='center' alignItems='center'>
-							<p style={{ color: 'white' }}>
+						<Grid item xs={12} xl={6} xlOffset={3} display='flex' justifyContent='center' alignItems='center'>
+							<Typography variant='body1' style={{ color: 'white' }}>
 								Row House is committed to helping you start a rowing-based fitness routine that will improve your health
 								and deliver unparalleled results. Move better, feel better and live better through Row House. BOOK YOUR
 								FIRST CLASS and join our community to PULL TOGETHER.
-							</p>
+							</Typography>
+						</Grid>
+
+						<div style={{ width: '100%', marginTop: 40 }}></div>
+
+						<Grid item xs={12} display='flex' justifyContent='center' alignItems='center'>
+							<Button size='large' variant='contained'>
+								Pre-Order Now
+							</Button>
 						</Grid>
 					</Grid>
 				</div>
